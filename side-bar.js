c987aae540483d48ce3f21c32fff1713c504aa2e
viditdangi <viditdangi18@gmail.com>
2026-06-19 02:48:52 +0530

    notes

--- side-bar.js
+++ side-bar.js
@@ -2,8 +2,6 @@ let menuIcon = document.querySelector(".menu-icon");
 let sidebar = document.querySelector(".sidebar");
 let container = document.querySelector(".container");
 
-// let menuIcon = document.querySelector(".menu-icon")
-
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");    
